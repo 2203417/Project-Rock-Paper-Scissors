3c46c51aa87d8dfa912c37d94b691b8d97f095cf
2203417 <la2203417@uabc.edu.mx>
2026-01-04 10:51:46 -0800

    dev changues

--- a.js
+++ a.js
@@ -2,10 +2,14 @@
 let btnRock = document.querySelector("#rock")
 let btnPaper = document.querySelector("#paper")
 let btnSiccors = document.querySelector("#siccors")
+let display = document.querySelector(".displayResult");
 
 function handleClick(e){
     console.log("it was clicked!", e.target.textContent);
+    playRound(e.target.dataset.value,getComputerChoice());
 }
+
+console.log(btnRock, btnPaper, btnSiccors);
 
 
 btnRock.addEventListener("click", handleClick);
@@ -18,40 +22,33 @@ function getComputerChoice(){
   return answer[Math.floor(Math.random() * 3)]
 }
 
-function getHumanChoice(){
-  play = play.toLocaleLowerCase();
-  if (play !== "rock" && play !== "paper" && play!== "scissors" ){
-    console.log("jugada invalida:(");
-    return null;
-  }
-  return play;
-}
 
 function playRound(humanSelection, computerSelection){
     if (humanSelection === computerSelection) {
     console.log("Empate!");
+    display.textContent = "Empate!";
   } 
   else if (humanSelection == 'rock' && computerSelection == 'scissors')
   {
     console.log('gano el humano');
+    display.textContent = "Ganó el humano";
   }else if (humanSelection == 'paper' && computerSelection == 'rock')
   {
     console.log('gano el humano');
+    display.textContent = "Ganó el humano";
   }else if (humanSelection == 'scissors' && computerSelection == 'paper')
   {
     console.log('gano el humano');
+    display.textContent = "Ganó el humano";
   }else{
     console.log('gano la computadora:(');
+    display.textContent = "Ganó la computadora 😢";
   }
 }
+
 
 
 var humanScore = 0;
 var computerScore = 0;
 
-const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
-
-
-
-playRound(humanSelection, computerSelection);
